e25a9fb898645ef4a7dbae30d6597e141cfcb6c5
Charles Zhang <progzhangchao@gmail.com>
2014-08-23 13:34:53 +0800

    rm os

--- os/д����ӳ��ָ����������/дָ����������/StdAfx.cpp
+++
@@ -1,8 +0,0 @@
-// stdafx.cpp : source file that includes just the standard includes
-//	写指定扇区工具.pch will be the pre-compiled header
-//	stdafx.obj will contain the pre-compiled type information
-
-#include "stdafx.h"
-
-
-
