b49360667fb213f23e9d7cfada4c951d7e385984
Charles Zhang <gis05zc@163.com>
2014-09-03 15:17:51 +0800

    根据不同情况处理编码转换

--- ruby/Util/helper.rb
+++ ruby/Util/helper.rb
@@ -1,13 +1,19 @@
 require 'rchardet'
 
 def toUtf8(_string)
-  return _string if _string.encoding == Encoding::UTF_8
-  cd = CharDet.detect(_string)
-  if cd["confidence"] > 0.6
-    _string.force_encoding(cd["encoding"])
+  case _string.encoding 
+  when Encoding::UTF_8 #utf-8不做处理
+  when Encoding::ASCII-8BIT
+    cd = CharDet.detect(_string)
+    if cd["confidence"] > 0.6
+      _string.force_encoding(cd["encoding"])
+    end
+    #_string.encode!("utf-8", :undef => :replace, :replace => "?", :invalid => :replace)
+    _string.encode!(Encoding::UTF_8)
+  else
+    _string.encode!(Encoding::UTF_8)
   end
-  #_string.encode!("utf-8", :undef => :replace, :replace => "?", :invalid => :replace)
-  _string.encode!(Encoding::UTF_8)
+
   return _string
 end
 
