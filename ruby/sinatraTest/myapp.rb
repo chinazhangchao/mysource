18866a668732bc56059d7db7872833a0514be01a
Charles Zhang <progzhangchao@gmail.com>
2014-09-21 09:52:19 +0800

    sinatra use database config

--- ruby/sinatraTest/myapp.rb
+++ ruby/sinatraTest/myapp.rb
@@ -5,12 +5,15 @@ require './config/init'
 
 configure do
   #set :server, :webrick
-  MongoMapper.setup(@config, @environment)
   set :server, :thin
   set :port, 80
+  MongoMapper.setup(@config, @environment)
 end
 
 get '/' do
   @test = "This is a test"
+  existing = DictionaryModel.where(:word => 'Brandon')
+  @dmArray=[]
+  existing.all.each{|e|dmArray << e.to_json}
   haml :index
 end
